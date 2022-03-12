5b67050299af88b7abeb5aaa778340add9abf500
Rasulov Farhad <farhad.rasulov@gt-m.ru>
2022-03-12 12:05:15 +0500

    Initial commit

---
+++ src/test/java/com/example/mypark/MyParkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mypark;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyParkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
